6e4091fe7c1afc374dced115e88f6516f9f25b9f
pintuSINGH2000 <pintusingh122000@gmail.com>
2022-09-20 13:13:49 +0530

    Style

--- routes/author.js
+++ routes/author.js
@@ -8,7 +8,7 @@ const Book=require('../model/book')
 router.get('/',async(req,res)=>{
     let searchOptions = {}
     if(req.query.name != null&& req.query.name !== ''){
-        searchOptions.name=new RegExp(req.query.name,'i');
+        searchOptions.name=new RegExp((req.query.name).trim(),'i');
     }
     try{
         const authors=await Author.find(searchOptions)
